3ef6fe027ab65e1edbf71faa4a902f23a94e9540
BlazeChron <51698874+BlazeChron@users.noreply.github.com>
2024-10-22 23:53:02 +0800

    Fix wrong image missing prompt

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
           <Image
             src={shimaenaga}
-            alt="Next.js logo"
+            alt="shimaenaga"
             width={180}
             height={38}
             priority
